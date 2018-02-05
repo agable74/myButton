75a5f71f91df0752f38ccad2f8697faa4c75a42c
Alex Gable <agable@ithaca.edu>
2018-02-05 14:40:59 -0500

    fixed buttons, starting sound

--- src/myNewGUI.java
+++ src/myNewGUI.java
@@ -1,8 +1,10 @@
-
+import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
 public class myNewGUI extends JFrame {
 
@@ -10,19 +12,22 @@ public class myNewGUI extends JFrame {
     private JButton custom;
     private ImageIcon notPress;
     private ImageIcon isPress;
+    private ImageIcon isHover;
 
     public myNewGUI(){
         super("");
         setLayout(new FlowLayout());
 
         notPress = createImageIcon("unpressed.png","my red button");
-        isPress = createImageIcon("pressed.png","my blue button");
+        isPress = createImageIcon("pressed.png","my clicked button");
+        isHover = createImageIcon("hovered.png","my blue button");
 
         custom = new JButton(notPress);
         custom.setBorderPainted(false);
         custom.setFocusPainted(false);
         custom.setContentAreaFilled(false);
         custom.setPressedIcon(isPress);
+        custom.setRolloverIcon(isHover);
         add(custom);
 
 
@@ -43,7 +48,37 @@ public class myNewGUI extends JFrame {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-
+            //attempts to play the sound file
+            try{
+                String sound = "Lab_11/data/victory.wav";
+                File audioFile = new File(sound);
+                AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+                AudioFormat format = audioStream.getFormat();
+                DataLine.Info info = new DataLine.Info(Clip.class, format);
+                Clip audioClip = (Clip) AudioSystem.getLine(info);
+                audioClip.open(audioStream);
+                audioClip.start();
+                //waits for full audio clip
+                try {
+                    Thread.sleep(3000);
+                }catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                audioClip.close();
+                audioStream.close();
+            }
+            //catch all the exceptions
+            catch(UnsupportedAudioFileException ex){
+                System.out.println("The specified audio file is not supported.");
+            }catch(LineUnavailableException ex){
+                System.out.println("Audio line for playing back is unavailable.");
+            }catch(IOException ex){
+                System.out.println("Error playing the audio file.");
+                ex.printStackTrace();
+            }catch(Exception ex){
+                System.out.println("Some other error occurring.");
+                ex.printStackTrace();
+            }
         }
     }
 }
--- src/myNewGUIMain.java
+++ src/myNewGUIMain.java
@@ -5,7 +5,7 @@ public class myNewGUIMain {
     public static void main(String[] args) {
         myNewGUI go = new myNewGUI();
         go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        go.setSize(new Dimension(300,300));
+        go.setSize(new Dimension(500,500));
         go.pack();
         go.setLocationRelativeTo(null);
         go.setVisible(true);
